b718ddb917a7f3a08db0f9834414d59269389ae2
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-03-28 21:18:46 +0300

    SOAP server fixed

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ from wsgiref.simple_server import server_version
 from flask import Flask, request
 from suds.client import Client
 
-app = Flask("__name__")
-server_uri = os.environ.get("SERVER_URI")
+app = Flask('app')
+server_uri = os.environ.get('SERVER_URI')
+client = Client(server_uri, cache=None)
 
 
 @app.route('/')
 def index():
     global server_uri
-    return f"Server URL: {server_uri}"
+    return f'Server URL: {server_uri}'
 
 
 @app.route('/showip', methods=['GET'])
 def host():
-    client = Client(server_uri, cache=None)
+    global server_uri
     domain_name = request.args.get('domain_name')
     if domain_name is None:
         domain_name = 'google.com'
@@ -25,12 +26,12 @@ def host():
 
 @app.route('/dns', methods=['GET'])
 def dns():
-    client = Client(server_uri, cache=None)
+    global server_uri
     domain_name = request.args.get('domain_name')
     if domain_name is None:
         domain_name = 'google.com'
     return client.service.more_info(domain_name)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
